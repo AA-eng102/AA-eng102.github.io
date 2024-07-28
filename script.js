1f9b7825733d2e55f867496b772eed4b61f9f213
AA-eng102 <etonuaaron2007@gmail.com>
2024-07-28 16:14:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example of interactive JavaScript (can be included in donate.html)
+document.getElementById('donateButton').addEventListener('click', function() {
+    alert('Thank you for your donation!');
+});
